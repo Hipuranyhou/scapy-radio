e516294ab8336f52719e565f890555a5b8ab4126
Hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2022-02-26 16:09:19 +0100

    Rewrote install.sh, blocks from blocks.txt are now installed in order from this file

--- install.sh
+++ install.sh
@@ -3,87 +3,79 @@
 # Copyright (C) Airbus DS CyberSecurity, 2014
 # Authors: Jean-Michel Picod, Arnaud Lebrun, Jonathan Christofer Demay
 
-base="$(pwd)"
-
 apply_patches() {
-  [ ! -d "${base}/patch/${1}" ] && return 
-  [ $(ls -A "${base}/patch/${1}" | wc -l) -eq 0 ] && return 
-  for p in ${base}/patch/${1}/*.patch; do
+  [ ! -d "${base_dir}/patch/${1}" ] && return 
+  oifs="$IFS"
+  IFS=$'\n'
+  for p in $(find "${base_dir}/patch/${1}" -type f); do
     patch -p0 <"${p}"
   done
+  IFS="${oifs}"
 }
 
-get_gr_block() {
-  cd "${base}/gnuradio"
-  block_name="$(basename ${1} | cut -d. -f1)"
+gr_block_name() {
+  echo "$(basename -s .git "${1}")"
+}
+
+gr_block_get() {
+  block_name="$(gr_block_name "${1}")"
   git clone "${1}"
   cd "${block_name}"
   git checkout maint-3.8
   cd ..
-  apply_patches "${block_name}"
-  cd "${base}"
-}
-
-scapy_install() {
-  cd "${base}"
-  git clone https://github.com/secdev/scapy.git
-  apply_patches scapy 
-  cd scapy
-  sudo python3 setup.py install
-  cd "${base}"
-}
-
-grc_install() {
-  cd "${base}"
-  mkdir -p "${HOME}/.scapy/radio/"
-  [ $(ls -A "gnuradio/grc" | wc -l) -eq 0 ] && return
-  for i in gnuradio/grc/*.grc; do
-    mkdir -p "${HOME}/.scapy/radio/$(basename ${i} .grc)"
-    cp "${i}" "${HOME}/.scapy/radio/"
-    if grcc -o "${HOME}/.scapy/radio/$(basename ${i} .grc)" "${i}"; then
-      # gnuradio 3.8 xmlrpc is not python3, we fix it for you :-)
-      sed -i 's/import SimpleXMLRPCServer/from xmlrpc.server import SimpleXMLRPCServer/g' "${HOME}/.scapy/radio/$(basename ${i} .grc)/top_block.py"
-      sed -i 's/SimpleXMLRPCServer.SimpleXMLRPCServer/SimpleXMLRPCServer/g' "${HOME}/.scapy/radio/$(basename ${i} .grc)/top_block.py"
-    fi
-  done
 }
 
 gr_block_install() {
   cd "${1}"
   mkdir -p build && cd build 
   cmake .. && make && sudo make install && sudo ldconfig
-  cd "${base}"
+  cd ../..
 }
 
-blocks_install() {
-  cd "${base}"
-  while IFS= read -r block; do
-    get_gr_block "${block}"
-  done <"gnuradio/blocks.txt"
-  for d in gnuradio/gr-*; do
-    gr_block_install "${d}"
-  done
-}
-if [ $# -eq 0 ]; then
-  scapy_install
-  blocks_install
-  grc_install
-else
-  while [ $# -ne 0 ]; do
-    case $1 in
-      scapy)
-	scapy_install
-	;;
-      grc)
-	grc_install
-	;;
-      blocks)
-	blocks_install
-	;;
-      *)
-	echo "Invalid option: $1"
-    esac
-    shift
-  done
-fi
+# "consts"
+base_dir="$(pwd)"
+build_dir="${base_dir}/build"
+grc_dir="${HOME}/.scapy/radio"
+
+# prepare directories
+mkdir -p "${build_dir}" "${grc_dir}"
+
+# install scapy
+cd "${build_dir}"
+git clone https://github.com/secdev/scapy.git
+apply_patches scapy 
+cd scapy
+sudo python3 setup.py install
+cd "${base_dir}"
+
+# install gnuradio blocks
+cd "${build_dir}"
+while IFS= read -r block_url; do
+  block_name="$(gr_block_name "${block_url}")"
+  gr_block_get "${block_url}"
+  apply_patches "${block_name}"
+  gr_block_install "${block_name}"
+done <../gnuradio/blocks.txt
+cp -r ../gnuradio/gr-scapy_radio .
+apply_patches gr-scapy_radio
+gr_block_install gr-scapy_radio
+cd "${base_dir}"
+
+# install gnuradio flowgraphs
+cd gnuradio/grc
+oifs="$IFS"
+IFS=$'\n'
+for g in $(find . -type f -name "*.grc"); do
+  grc_name="$(basename -s .grc "${g}")"
+  grc_path="${grc_dir}/${grc_name}"
+  mkdir "${grc_path}"
+  cp "${g}" "${grc_path}"
+  if grcc -o "${grc_path}" "${g}"; then
+    # gnuradio 3.8 xmlrpc is not python3, we fix it for you :-)
+    sed -i 's/import SimpleXMLRPCServer/from xmlrpc.server import SimpleXMLRPCServer/g' "${grc_path}/top_block.py"
+    sed -i 's/SimpleXMLRPCServer.SimpleXMLRPCServer/SimpleXMLRPCServer/g' "${grc_path}/top_block.py"
+  fi
+done
+IFS="${oifs}"
+cd "${base_dir}"
 
